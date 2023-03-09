812f413bf5961b43e8623c8d8d3e9f26b53946fb
FelipeMFOffice <femf20795@gmail.com>
2023-03-09 17:49:06 -0300

    Exercicio 1 JS

---
+++ Aulas Modulo 01/index.js
@@ -0,0 +1,18 @@
+let nome = "FelipeMasterDev!!"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+/*
+window.alert(nome)
+nome = window.prompt("Insira seu nome:")
+let confirmacao = window.confirm("Confirma essa ação?")
+*/
+//Não é necessario utilizar o comando Window quando é para navegador.
+
+alert(nome)
+nome = prompt("Insira seu nome:")
+confirm("Confirma esse trem ai ?")
+
+parseFloat("42") //converte String para Number
---
+++ Exercicio 01/index.js
@@ -0,0 +1,14 @@
+let primeiroNome = undefined
+let segundoNome = undefined
+let campoDeEstudo = undefined
+let anoNascimento = undefined
+
+primeiroNome = prompt ("Insira seu primeiro Nome:")
+segundoNome = prompt ("Insira seu segundo Nome:")
+campoDeEstudo = prompt ("Insira seu campo de Estudo:")
+anoNascimento = prompt ("Insira seu ano de nascimento:")
+
+alert(primeiroNome)
+alert(segundoNome)
+alert(campoDeEstudo)*/
+alert((2023 - anoNascimento) + " Anos")
