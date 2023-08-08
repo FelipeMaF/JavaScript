519e9d1b8a4b6ba6ffadc10a1fb6de07a4bcd935
FelipeCasa <femf20795@gmail.com>
2023-08-07 21:31:59 -0300

    Atualizando 07082023 2132

---
+++ B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Métodos de Numbers/metodosNumbers.js
@@ -0,0 +1,9 @@
+let n = 10.456
+let n1 = '5'
+
+let num = n
+
+alert(num.toString())//Converte o numero para String
+alert(num.toFixed(2))//Define o numero de decimais exibido
+alert(num.toFixed(3))//Define o numero de decimais exibido(e arredonda)
+alert(('Sem parseInt: '+ (n1 + n)) + ('  Com parseInt: ' + (parseInt(n1) + n)))//converte string para number
--- B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Métodos de String/metodosString.js
+++ B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Métodos de String/metodosString.js
@@ -1,8 +1,30 @@
 let nome = 'Felipe Matheus Fraga'
+let nome2 = '      Usando o'
+/*
+//Contagem de caracteres
 alert(nome.length + '   .lenght') //mostra quantos caracteres contando espaços
 
+//BUSCANDO CONTEUDO
 alert(nome.indexOf('Matheus') + '   indexOf - posição do Matheus') //mostra se existe este conteudo no texto, retornando como resultado o numero da posição, neste caso seria o 7, pois o Matheus vem depois da 7º posição, mas ele pesquisa também por apenas uma letra ou numero, se fosse 't' ele traria 9. Se não tiver, retorna -1
-
 alert(nome.indexOf('t') + '   indexOf - Posição do t')
-
 alert(nome.indexOf('z') + '   indexOf - procurando por Z retorna -1 pois não achou')
+
+//RECORTANDO CONTEUDO
+alert(nome.slice(0, 6) + '   .slice') //o slice seleciona oque deseja exibir através das posições
+alert(nome.slice(7, 14)+ '   .slice')
+alert(nome.slice(15)+ '   .slice')//ou inicia a partir de uma posição citada
+alert(nome.slice(-10)+ '   .slice')//pode pegar de trás pra frente com numero negativo
+//alert(nome.substring(0, 5)) faz a mesma coisa do slice, só que tem menos recursos.
+alert(nome.substr(0, 5)+ '   .slice')// o substr vc escolhe de onde vai começar a pegar, e quantos caractes vai mostrar.
+*/
+
+//SUBSTITUINDO CONTEUDO
+alert(nome.replace('Fraga', 'Praga') + '   .replace')//O replace Substitui (X por Y), apenas substitui
+
+alert(nome.toUpperCase() + '   .topperCase')//tudo maiusculo
+alert(nome.toLowerCase() + '   .toLowerCase')//tudo minusculo
+alert(nome.concat(' o', ' Cara') + '   .concat')//concatenar
+alert(nome2.trim() + '   .trim')//remove os espaços do começo
+alert(nome.charAt('7') + '   .charAt(7)')//ve oq tem nessa posição, no caso 'M'
+alert(nome[7] + '   variavel[7]')//faz a mesma coisa do charAt
+alert(nome.split(' ') + ' <- isso é um Array')//Cria um array separando através do atributo citado, no exemplo foi espaço, mas poderia ser ; , . entre outros.
