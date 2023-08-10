ed754fe7977347bce510ebb6cbb2ca201bfd89e1
FelipeCasa <femf20795@gmail.com>
2023-08-09 21:55:58 -0300

    09082023 2156

--- B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Datas/datas.js
+++ B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Datas/datas.js
@@ -1,7 +1,19 @@
 let d = new Date()
 let da = new Date('01/01/0101 17:26')
 
-alert(d)
-alert(d.toDateString())
-alert(d.toUTCString())
-alert(da)
+console.log(d)
+console.log(d.toDateString())
+console.log(d.toUTCString())
+console.log(da)
+d.setFullYear(2022)//edita o ano
+console.log(d)
+d.setMonth(10)//edita o mes
+console.log(d)
+d.setDate(15)//edita o dia
+console.log(d)
+
+//PRAZO
+d.setDate(d.getDate() + 5)//pega a data de hoje e adiciona a quantidade de dias
+console.log(d)
+d.setHours(d.getHours() + 5)//Pega a hora atual e adiciona a quantidade de horas
+console.log(d)
---
+++ B7Web/Mod 9 Manipulação-Arrow-Ajax etc/Matematica/matematica.js
@@ -0,0 +1,12 @@
+//ARREDONDAMENTO
+let v1 = Math.round(3.56)//arredonda para cima ou baixo
+let v2 = Math.floor(3.56)//arredonda para baixo
+let v3 = Math.ceil(3.56)//arredonda para cima
+
+//BUSCAR O MAXIMO E O MINIMO
+let v4 = Math.min(7, 100, 200, 58, 96, 5, 15)//retorna o menor numero da lista
+let v5 = Math.min(7, 100, 200, 58, 96, 5, 15)//retorna o maior numero da lista
+
+//NUMEROS ALEATORIOS
+let v6 = Math.random()//retorna um numero aleatorio entre 0 e 1 (0.195687555)
+let v7 = Math.floor(math.random() *100)//assim ele retorna um numero aleatorio entre 0 e 100
